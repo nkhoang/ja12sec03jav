41b47b6043db1d17039f85e8145d7d1fcc943b9c
Hoang Nguyen Khanh <hoangknguyen.it@gmail.com>
2013-02-13 23:49:16 +0700

    - Create a new WS service.

--- web/src/main/java/com/googleappengine/webservice/RestfulService.java
+++ web/src/main/java/com/googleappengine/webservice/RestfulService.java
@@ -3,28 +3,22 @@ package com.googleappengine.webservice;
 import com.googleappengine.model.WordEntity;
 import com.googleappengine.service.WordService;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.List;
 
 @Service(value = "restfulService")
 @Path("/")
 public class RestfulService {
-    private static Logger LOG = LoggerFactory.getLogger(RestfulService.class.getCanonicalName());
-    private static StringBuilder sb = new StringBuilder();
+    private static Logger LOG = LoggerFactory.getLogger(WebToolService.class.getCanonicalName());
 
     @Autowired
     private WordService wordService;
@@ -66,64 +60,6 @@ public class RestfulService {
         return "nothing found";
     }
 
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    @Path("loadcities")
-    @CrossOriginResourceSharing(allowAllOrigins = true)
-    public String loadCities(@QueryParam("queryStr") String queryStr, @QueryParam("url") String urlStr) {
-        try {
-            if (StringUtils.isBlank(urlStr)) {
-                urlStr = "http://thoitrangdshop.vn/loadselect_ajax.aspx";
-            }
-            URL url = new URL(urlStr);
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-            connection.setRequestProperty("Accept", "*/*");
-            connection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
-            connection.setRequestMethod("POST");
-
-            //Send request
-            if (StringUtils.isNotBlank(queryStr)) {
-                DataOutputStream wr = new DataOutputStream(
-                        connection.getOutputStream());
-                wr.writeBytes(queryStr);
-                wr.flush();
-                wr.close();
-            }
-            // get inputStream
-            InputStream is = connection.getInputStream();
-
-            String s = new String(IOUtils.toByteArray(is), Charset.forName("UTF-8"));
-
-            return s;
-        } catch (Exception ex) {
-            LOG.debug("Something wrong.", ex);
-        }
-        return null;
-    }
-
-    @POST
-    @Path("sendlog")
-    @Produces(MediaType.TEXT_HTML)
-    @CrossOriginResourceSharing(allowAllOrigins = true)
-    public String sendLog(@FormParam(value = "log") String log) {
-        LOG.info(log);
-        sb.append(log);
-        return "ok";
-    }
-
-    @GET
-    @Path("getlog")
-    @Produces(MediaType.TEXT_HTML)
-    @CrossOriginResourceSharing(allowAllOrigins = true)
-    public String getLog() {
-        String result = sb.toString();
-        sb = new StringBuilder();
-
-        return result;
-    }
 }
 
